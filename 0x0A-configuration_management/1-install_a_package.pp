442fa0240617c9c348378bbbd0536016c5393662
james-eo <jameseokonkwo@gmail.com>
2024-03-22 14:12:26 +0100

    Flask installation

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# This file installs flask from pip3
+# This file installs 'flask' from pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure => 'installed',
 }
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  creates => '/usr/local/lib/python3.10/dist-packages/flask',
+}
+
