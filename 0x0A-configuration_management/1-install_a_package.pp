1fe506c209f5d0e74aad768bdb1ee294043c90ee
james-eo <jameseokonkwo@gmail.com>
2024-03-29 10:00:12 +0100

    Installing Falsk

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,6 @@
 # This file installs 'flask' from pip3
 
-#package { 'flask':
-#  ensure   => '2.1.0',
-#  provider => 'pip3',
-#}
-
-package {'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  creates => '/usr/local/lib/python3.8/dist-packages/flask',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
