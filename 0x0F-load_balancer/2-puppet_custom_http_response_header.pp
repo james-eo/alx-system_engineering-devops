2224dafdac8ebcfa3d809fcf6bc10b2632de2a8e
james-eo <jameseokonkwo@gmail.com>
2024-04-01 21:43:37 +0100

    Add a custom HTTP header with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,18 @@
+# Automate the task of creating a custom HTTP header response with Puppet.
+
+include stdlib
+
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package {'nginx':
+  ensure => 'present',
+}
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
+}
