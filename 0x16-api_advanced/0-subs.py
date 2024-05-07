e0b1ca1863ab45c22579aab6dc9e9b9abbd2840c
james-eo <jameseokonkwo@gmail.com>
2024-05-07 23:09:57 +0100

    Query Reddit API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -22,7 +22,7 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        subscribers = response.json().get("data").get("subscribers")
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
         return subscribers
-    else:
-        return 0
+    return 0
