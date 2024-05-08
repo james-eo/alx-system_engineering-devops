ab67b069b7d610e1b4d44c64b5c7a2d6addd3e72
james-eo <jameseokonkwo@gmail.com>
2024-05-08 14:12:17 +0100

    Web stack debugging (Wordpress debugging)

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# Fixes bad php extensions from 'phpp' to `php` in the WordPress file `wp-settings.php`
+
 file { '/var/www/html/wp-settings.php':
 ensure  => present,
 content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub(/\.phpp/, ".php") %>'),
