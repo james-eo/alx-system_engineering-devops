23a1670400df9fda7262ec3e825b3dfb92aeb082
james-eo <jameseokonkwo@gmail.com>
2024-05-08 08:50:18 +0100

    Queries Reddit API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,13 +16,18 @@ def number_of_subscribers(subreddit):
         The number of subscribers for the subreddit.
         If the subreddit is invalid, returns 0.
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json?limit=0"
     headers = {"User-Agent": "Mozilla/5.0"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
-    return 0
+    if response.status_code == 404:
+        return 0
+    data = response.json().get("data")
+    return data.get("subscribers")
+
+#    if response.status_code == 200:
+#        data = response.json()
+#        subscribers = data["data"]["subscribers"]
+#        return subscribers
+#    return 0
